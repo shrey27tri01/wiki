4b8d441d47bf7c4798782f8e6a4fd1820e592027
shrey27tri01 <shrey.tripathi@iiitb.org>
2020-10-23 11:09:21 +0530

    Changed deatabase to postgres

--- wiki/settings.py
+++ wiki/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'wiki.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'd8mlihv9emgkoo',
+        'HOST': 'ec2-3-233-236-188.compute-1.amazonaws.com',
+        'PORT': 5432,
+        'USER': 'licnfyjerjbqij',
+        'PASSWORD': '2d1e42e4a84db6d10c0ce0641b02d79f026bba75744658ca7a60dae714b92527'
     }
 }
 
